9f815011c70c4464d0a94775df00c894bfa813d4
Thái Minh Sang <sang024854@gmail.com>
2017-12-23 17:08:58 +0700

    Chỉnh giao diện đăng nhập/đăng ký

--- views/_layout.php
+++ views/_layout.php
@@ -26,6 +26,7 @@ if ($_SESSION["ttdn"] == 0) {
 		<link rel="stylesheet" href="assets/bootstrap4.min.css" >
 		<link rel="stylesheet" type="text/css" href="assets/style.css">
 		<link rel="stylesheet" type="text/css" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 	</head>
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -204,110 +205,54 @@ if ($_SESSION["ttdn"] == 0) {
 					<div class="modal-content">
 						<br>
 						<div class="bs-example bs-example-tabs">
-							<ul id="myTab" class="nav nav-tabs">
-								<li class="active"><a href="#signin" data-toggle="tab">Đăng nhập</a></li>
-								<div> | </div>
-								<li><a href="#signup" data-toggle="tab">Đăng ký</a></li>
+							<ul id="myTab" class="nav nav-tabs" role="tablist">
+								<li class="nav-item">
+									<a class="nav-link active" href="#signin" data-toggle="tab" role="tab">Đăng nhập</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#signup" data-toggle="tab" role="tab">Đăng ký</a>
+								</li>
 							</ul>
 						</div>
 						<div class="modal-body">
-							<div id="myTabContent" class="tab-content">
-								<div class="tab-pane fade active in" id="signin">
-									<form class="form-horizontal" name="fdangnhap" action="xuly.php" method="post">
-										<fieldset>
-											<!-- Sign In Form -->
-											<!-- Text input-->
-											<div class="control-group">
-												<label class="control-label" for="userid">Tên tài khoản:</label>
-												<div class="controls">
-													<input required="" id="userid" name="userid" type="text" class="form-control" placeholder="sangdeptrai" class="input-medium" required="">
-												</div>
-											</div>
-											<!-- Password input-->
-											<div class="control-group">
-												<label class="control-label" for="passwordinput">Mật khẩu:</label>
-												<div class="controls">
-													<input required="" id="passwordinput" name="passwordinput" class="form-control" type="password" placeholder="********" class="input-medium">
-												</div>
-											</div>
-											<!-- Multiple Checkboxes (inline) -->
-											<div class="control-group">
-												<label class="control-label" for="rememberme"></label>
-												<div class="controls">
-													<label class="checkbox inline" for="rememberme-0">
-														<input type="checkbox" name="rememberme" id="rememberme" value="Remember me">
-														Ghi nhớ tài khoản
-													</label>
-												</div>
-											</div>
-											<!-- Button -->
-											<div class="control-group">
-												<label class="control-label" for="signin"></label>
-												<div class="controls">
-													<button id="signin" name="signin" class="btn btn-success">Đăng nhập</button>
-												</div>
-											</div>
-										</fieldset>
+							<div id="myTabContent" class="tab-content active">
+								<div class="tab-pane fade active show" id="signin">
+									<form class="form-horizontal" method="post" id="loginForm">
+										<input type="text" id="logUsername" class="form-control" placeholder="Tên đăng nhập" autocomplete="on" autofocus>
+										<br>
+										<input type="password" id="logPass" class="form-control" placeholder="Mật khẩu">
+										<br>
+										<div class="form-check">
+											<label class="form-check-label">
+												<input id="btnLog" type="checkbox" class="form-check-input" value="remember-me">Lưu đăng nhập
+											</label>
+										</div>
+										<button type="button" class="btn btn-success">Đăng nhập</button>
 									</form>
 								</div>
 								<div class="tab-pane fade" id="signup">
-									<form class="form-horizontal" name="fdangky" action="xuly.php" method="post">
-										<fieldset>
-											<!-- Sign Up Form -->
-											<!-- Text input-->
-											<div class="control-group">
-												<label class="control-label" for="Email">Email:</label>
-												<div class="controls">
-													<input id="Email" name="Email" class="form-control" type="text" placeholder="tuansang9710@gmail.com" class="input-large" required="">
-												</div>
-											</div>
-											
-											<!-- Text input-->
-											<div class="control-group">
-												<label class="control-label" for="userid">Tên tài khoản:</label>
-												<div class="controls">
-													<input id="userid" name="userid" class="form-control" type="text" placeholder="sangdeptrai" class="input-large" required="">
-												</div>
-											</div>
-											
-											<!-- Password input-->
-											<div class="control-group">
-												<label class="control-label" for="password">Mật khẩu:</label>
-												<div class="controls">
-													<input id="password" name="password" class="form-control" type="password" placeholder="********" class="input-large" required="">
-												</div>
-											</div>
-											
-											<!-- Text input-->
-											<div class="control-group">
-												<label class="control-label" for="reenterpassword">Nhập lại mật khẩu:</label>
-												<div class="controls">
-													<input id="reenterpassword" class="form-control" name="reenterpassword" type="password" placeholder="********" class="input-large" required="">
-												</div>
-											</div>
-											
-											<!-- Multiple Radios (inline) -->
-											<br>
-											<div class="control-group">
-												<label class="control-label" for="humancheck">Humanity Check:</label>
-												<div class="controls">
-													<label class="radio inline" for="humancheck-0">
-														<input type="radio" name="humancheck" id="humancheck-0" value="robot" checked="checked">I'm a Robot
-													</label>
-													<label class="radio inline" for="humancheck-1">
-														<input type="radio" name="humancheck" id="humancheck-1" value="human">I'm Human
-													</label>
-												</div>
-											</div>
-											
-											<!-- Button -->
-											<div class="control-group">
-												<label class="control-label" for="confirmsignup"></label>
-												<div class="controls">
-													<button id="signup" name="signup" class="btn btn-success">Đăng ký</button>
-												</div>
-											</div>
-										</fieldset>
+									<form class="form-horizontal" method="post" id="regForm">
+										<input type="email" id="regEmail" class="form-control" placeholder="Email">
+										<div id="nd"></div>
+										<br>
+										<input type="text" id="regUsername" class="form-control" placeholder="Tên tài khoản">
+										<br>
+										<input type="password" id="regPass" class="form-control" placeholder="Mật khẩu">
+										<br>
+										<input type="password" id="regRePass" class="form-control" placeholder="Nhập lại mật khẩu">
+										<br>
+										<div class="g-recaptcha" data-sitekey="6Ld9zjgUAAAAAHpQ35RNeLBFfzZHsr2GmHA9xrnS"></div>
+										<br>
+										<div class="form-check">
+											<label class="form-check-label">
+												<input id="btnRegCheckbox" type="checkbox" class="form-check-input" value="remember-me">Bạn có đồng ý với điều khoản củ *** này ko?
+											</label>
+										</div>
+										<br>
+										<button id="btnReg" type="button" class="btn btn-success">
+											Đăng ký
+										</button>
+										
 									</form>
 								</div>
 							</div>
